d41fc161706e98aabdca6128d553908df07bc9a4
yukyongsoo <yozx234@gmail.com>
2021-11-10 03:06:56 +0900

    fix test shutdown too slow

--- src/main/kotlin/com/yuk/debating/RedisConfig.kt
+++ src/main/kotlin/com/yuk/debating/RedisConfig.kt
@@ -1,27 +1,28 @@
 package com.yuk.debating
 
-import io.lettuce.core.ClientOptions
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import java.time.Duration
 
 
 @Configuration
 class RedisConfig {
 
     @Bean
-    fun getRedisConfig(): RedisConnectionFactory {
-        val redisStandaloneConfiguration = RedisStandaloneConfiguration()
-
-        val clientOptions = ClientOptions.builder()
-            .autoReconnect(false)
-            .build()
+    fun getRedisConfig(
+        @Value("\${spring.redis.host}") hostName: String,
+        @Value("\${spring.redis.port}") port: Int
+    ): RedisConnectionFactory {
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration(hostName, port)
 
         val clientConfig = LettuceClientConfiguration.builder()
-            .clientOptions(clientOptions)
+            .commandTimeout(Duration.ofMillis(500))
+            .shutdownTimeout(Duration.ofMillis(500))
             .build()
 
         return LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig)
